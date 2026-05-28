21aa21c99812393de2ff98140ce97d63a0c361a3
Victor Ruiz <ruiz.victorvieira@gmail.com>
2026-05-28 15:24:09 -0300

    feat: implementa estado global com Provider e ChangeNotifier

---
+++ lib/state/app_state.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/foundation.dart';
+
+import '../data/models/debris.dart';
+import '../data/models/risk_level.dart';
+import '../data/repositories/debris_repository.dart';
+
+class AppState extends ChangeNotifier {
+  RiskLevel? _selectedRisk;
+  final Set<String> _monitoredIds = <String>{};
+
+  RiskLevel? get selectedRisk => _selectedRisk;
+
+  Set<String> get monitoredIds => Set.unmodifiable(_monitoredIds);
+
+  int get monitoredCount => _monitoredIds.length;
+
+  List<Debris> get filteredDebris {
+    final risk = _selectedRisk;
+    if (risk == null) {
+      return DebrisRepository.all;
+    }
+    return DebrisRepository.all
+        .where((debris) => debris.riskLevel == risk)
+        .toList(growable: false);
+  }
+
+  bool isMonitored(String id) => _monitoredIds.contains(id);
+
+  void selectRisk(RiskLevel? risk) {
+    if (_selectedRisk == risk) return;
+    _selectedRisk = risk;
+    notifyListeners();
+  }
+
+  void toggleMonitored(String id) {
+    if (!_monitoredIds.add(id)) {
+      _monitoredIds.remove(id);
+    }
+    notifyListeners();
+  }
+}
